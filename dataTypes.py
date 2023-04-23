3115fd3c83a8292b1c3ca8d4a350619fef8541d8
Raushan <raushankcse@gmail.com>
2023-04-23 20:26:00 +0530

    added some program in dataTypes code.

--- dataTypes.py
+++ dataTypes.py
@@ -116,38 +116,171 @@ print(type(c))
 
 
 # generating random number
-
+'''
 import random
 
 print(random.randrange(1,10))
+'''
 
 
 
+# #####################Strings data types
+'''
+a="Hello"
+print(a)
+'''
+
+# multiline strings
+'''
+a = """ it is 
+a 
+multiline string.
+"""
+
+print(a)
+'''
+
+"""
+a = '''It is 
+a 
+multiline
+string
+'''
+print(a)
+
+"""
+
+
+# strings are work like arrays
+'''
+a= "Hello, World!"
+print(a[1])
+'''
+
+# string looping
+'''
+for x in "banana":
+	for i in x:
+		print(i,end="")
+	print()
+
+'''
+
+
+# length of the string
+'''
+a = "Hello, World!"
+print(len(a))
+'''
+
+
+# Check String is in or not
+'''
+txt = "The best things in life are free!"
+print("free" in txt)
+'''
+
+'''
+txt = "The best things in life are free!"
+if "free" in txt:
+	print("Yes, 'free' is present.")
+'''
+
+'''
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+'''
+
+
+'''
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+	print("No, 'expensive' is NOT present. ")
+'''
+
+
+# string method
+'''
+b = "Hello, World!"
+print(b[2:5])
+'''
+
+'''
+b = "Hello, World!"
+print(b[:5])
+'''
+
+'''
+b = "Hello, World!"
+print(b[2:])
+'''
+
+'''
+b = "Hello, World!"
+print(b[-5:-2])
+'''
+
+
+'''
+a = "Hello, World!"
+print(a.upper())
+'''
+
+'''
+a = "   Hello, World!"
+print(a.strip())
+'''
 
 
 
+'''
+a = "Hello, World!"
+print(a.replace("H", "J"))
+'''
 
 
 
+'''
+a = "Hello, World!"
+print(a.split(","))
+'''
+
+'''
+a = "Hello"
+b = "World"
+c = a+" " +b
+print(c)
+'''
 
 
+'''
+age = 36
+txt = "My name is John, I am {}" 
+print(txt.format(age))
+'''
+
+'''
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+'''
+
+'''
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}. "
+print(myorder.format(quantity, itemno, price))
+'''
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#escape strings
+'''
+txt = "We are the so-called \" Vikings\" from the north."
+print(txt)
+'''
 
 
 
