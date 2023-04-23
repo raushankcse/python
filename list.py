7cb29f5517e92c591c73961d920c5423c402fd24
Raushan <raushankcse@gmail.com>
2023-04-23 22:16:45 +0530

    list

---
+++ list.py
@@ -0,0 +1,34 @@
+
+
+# list in python
+'''
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+'''
+
+bikes = ['trek', 'redline', 'giant']
+first_bike = bikes[0]
+last_bike = bikes[-1]
+
+
+
+
+
+
+
+print(first_bike)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ operator.py
@@ -0,0 +1,143 @@
+
+
+# Operators in python
+
+#arithmetic operator
+'''
+print(10+5)
+print(10-4)
+print(10*4)
+print(10/3)
+print(10%3)
+print(2**2)
+print(5//3)
+'''
+
+
+#assignment operator
+'''
+a=4
+print(a)
+
+a+=3
+print(a)
+a-=3
+print(a)
+a*=3
+print(a)
+a/=3
+print(a)
+a%=3
+print(a)
+a//=3
+print(a)
+a**=3
+print(a)
+'''
+
+
+
+# Python comparison operators
+
+'''
+a=4
+b=5
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+'''
+
+
+# Python Logical Operators
+'''
+a= 4
+
+
+print(a<5 and a<10)
+print(a<6 or a>3)
+print(not(a<5 and a>10))
+'''
+
+
+#python identity operator
+
+'''
+a=4
+b=4
+
+print(a is b)
+print(a is not b)
+'''
+
+
+# python membership operator
+'''
+txt = "hello guys, i'm raushan kumar"
+print("raushan" in txt)
+print("hii" not in txt)
+'''
+
+
+# python bitwise operators.
+'''
+x = 5
+y = 6
+
+print(x & y)
+print(x | y)
+print(x^y)
+print(~x)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
