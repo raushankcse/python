65918e00c985e52d19216611ce9bd014d01020ad
Raushan <raushankcse@gmail.com>
2023-04-25 21:44:58 +0530

    updated list

--- list.py
+++ list.py
@@ -5,10 +5,40 @@
 mylist = ["apple", "banana", "cherry"]
 print(mylist)
 '''
-
+'''
 bikes = ['trek', 'redline', 'giant']
 first_bike = bikes[0]
 last_bike = bikes[-1]
+for bike in bikes:
+	print(bike)
+
+
+'''
+
+'''
+bikes = []
+bikes.append('trek')
+bikes.append('redline')
+bikes.append('giant')
+
+print(bikes)
+
+'''
+
+'''
+squares = []
+for x in range(1,11):
+	squares.append(x**2)
+
+
+print(squares)
+
+'''
+
+#List comprehension
+
+
+squares = [x**2 for x in range(1,11))
 
 
 
@@ -16,7 +46,20 @@ last_bike = bikes[-1]
 
 
 
-print(first_bike)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
